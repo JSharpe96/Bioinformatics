b2b4d5a1f27b8f45180a7c01f2bfa81122376cba
JSharpe96 <56839382+JSharpe96@users.noreply.github.com>
2024-02-08 18:32:01 -0500

    Hw06

--- Code/Homework06.R
+++ Code/Homework06.R
@@ -1,7 +1,7 @@
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install("Biostrings")
-BiocManager::install("GenomicAlignments")
+BiocManager::install("Biostrings", force = TRUE)
+BiocManager::install("GenomicAlignments", force = TRUE)
 library(Biostrings)
 library(msa)
 library(dplyr)
@@ -11,16 +11,46 @@ library(tidyr)
 install.packages("seqinr")
 library(seqinr)
 library(ape)
-install.packages(UniprotR)
-install.packages(protti)
-install.packages(r3dmol)
+install.packages("UniprotR")
+install.packages("protti")
+install.packages("r3dmol")
+library(UniprotR)
+library(protti)
+library(r3dmol)
+library(phangorn)
 
 
-setwd("Data/Homework06/")
 #Set Working directory for Data and Homework folder ####
+setwd("GitHub/Bioinformatics/Data/Homework06/")
 
-getwd()
+
 #Checked that directory was in place ####
+getwd()
 
-mySequences01 <- readDNAStringSet("sequence01.fasta")
+mySequences01 <- readDNAStringSet("Homework06/sequence01.fasta")
+
+
+#### Translate DNA sequence into AA sequence ####
+dna_sequences <- readDNAStringSet("Homework06/sequence01.fasta")
+aa_sequence <- translate(dna_sequences)
+aa_sequence
+as.character(aa_sequence)
+
+#### Writing a aa sequence into a fasta file ####
+?writeXStringSet()
+writeXStringSet(aa_sequence)
+
+readAAStringSet(filepath = "Homework06", format="fasta",
+                nrec=-1L, skip=0L, seek.first.rec=FALSE,
+                use.names=TRUE, with.qualities=FALSE)
+
+output_file <- "Homework06/amino_acid_sequence.fasta"
+
+# Write the amino acid sequence to a .fasta file
+writeXStringSet(aa_sequence, file = output_file,
+                format = "fasta", width = 60)
+
+
+
+
 
