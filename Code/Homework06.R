8d9426040b3dec62a926cf1c50cca2952f77ec68
JSharpe96 <56839382+JSharpe96@users.noreply.github.com>
2024-02-09 19:10:32 -0500

    Hw6

--- Code/Homework06.R
+++ Code/Homework06.R
@@ -19,7 +19,6 @@ library(protti)
 library(r3dmol)
 library(phangorn)
 
-
 #Set Working directory for Data and Homework folder ####
 setwd("GitHub/Bioinformatics/Data/Homework06/")
 
@@ -40,6 +39,35 @@ as.character(aa_sequence)
 output_file <- "Homework06/amino_acid_sequence.fasta"
 writeXStringSet(aa_sequence, file = output_file,
                 format = "fasta", width = 60)
+
+#### Read this file into R using the appropriate function ####
+accession_numbers<- read.table("Homework06/AccNumbers.txt")
+
+# Sample list of accession numbers
+#accession_numbers <- c("AYN72248", "AYN72247", "AYN72246", "AYN72242", "KAF0875111")
+
+# Convert the list to a character string
+#accession_string <- paste(accession_numbers, collapse = ",")
+
+# Print the formatted string
+#print(accession_string)
+
+
+AccessionNumbersGO <- GetProteinGOInfo(accession_numbers)
+PlotGoInfo(AccessionNumbersGO)
+?PlotGoInfo()
+
+# Connect to UniProt database
+up <- UniProt.ws(accession_numbers)
+
+# Query UniProt database to retrieve protein entries
+protein_entries <- getBM(attributes = c('acc', 'go_id'),
+                         filters = 'acc',
+                         values = accession_numbers,
+                         mart = up)
+
+# Show the retrieved GO terms
+print(protein_entries)
 
 
 
